e0534156ad2e263f11cb9791033fa81363c1d849
Motta <lucascriciuma@gmail.com>
2022-05-10 00:10:47 -0300

    Ajustes

---
+++ projeto/src/test/java/com/projeto/projeto/Utils/CarregaArquivoTest.java
@@ -0,0 +1,26 @@
+package com.projeto.projeto.Utils;
+
+import com.projeto.projeto.business.PremiacaoBusiness;
+import com.projeto.projeto.model.entity.Premiacao;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CarregaArquivoTest {
+
+
+
+    @BeforeEach
+    public void setUp(){
+
+    }
+    
+    @Test
+    public void test(){
+
+    }
+
+}
